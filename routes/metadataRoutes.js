8a737d4975a5e301c9276c844a9d5089015ad4d1
mranoncoder <inbox@abolfazl.de>
2023-11-04 13:10:10 +0100

    ✨

--- routes/metadataRoutes.js
+++ routes/metadataRoutes.js
@@ -13,15 +13,19 @@ const {
   getKeyTokensByID
 } = require("../controllers/tokenController");
 
+
+router.get("/lootbox", getAllLootBoxes);
 router.get("/lootbox/:tokenID", getLootBoxTokenByID);
 router.get("/multiple/lootboxes", getLootBoxTokensByID);
-router.get("/lootbox", getAllLootBoxes);
 router.get("/info/lootbox/:boxID", getAllLootBoxInfo);
-router.get("/key/:tokenID", getItemTokenByID);
-router.get("/multiple/keys", getKeyTokensByID);
-router.get("/key", getAllKeys);
-router.get("/info/key/:boxID", getOneKeyInfo);
+
 router.get("/item/:tokenID", getKeyTokenByID);
 router.get("/multiple/items", getItemTokensByID);
+
+
+router.get("/key", getAllKeys);
+router.get("/key/:tokenID", getKeyTokenByID);
+router.get("/multiple/keys", getKeyTokensByID);
+router.get("/info/key/:boxID", getOneKeyInfo);
 
 module.exports = router;
